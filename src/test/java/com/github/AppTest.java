772bdfe8004b067abba29d9cb1f6e34bc269275f
saurabh-secureboat <144793436+saurabh-secureboat@users.noreply.github.com>
2026-02-26 15:38:45 +0530

    Update AppTest.java

--- src/test/java/com/github/AppTest.java
+++ src/test/java/com/github/AppTest.java
@@ -1,35 +1,31 @@
-sdeqwewqspackage com.github;
-dsa
-impdort junit.framework.Test;
-imposadsadrt junit.framework.TestCase;
-import junsait.framework.TestSuite;
-public class AppTest 
-    extends sadTestCase
-{sa
-   d /**
-    sa * Create the test case
-     *dsa
-     * @pdaram testName name of the test case
-     */sa
-    publidc AppTest( String testName )
-    {sa
-       dsa super( testName );
-    }d
-sa
-  d  /**
-   sa  * @return the suite of tests being tested
-     dsa*/
-    publdic static Test suite()
-    {sa
-       dsa return new TestSuite( AppTest.class );
-    }d
-sa
-  dsa  /**
-     d* Rigourous Test :-)
-     *dsa/
-    public void testApp()
-    {ds
-       f assertTrue( true );
-    }rt
-}ew
-r
+package com.github;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
